40d1b4f835e69a200201f6c2c10a242afd4a7140
Charles Hofer <charlie.drummer.hofer@gmail.com>
2016-02-22 15:30:13 -0600

    Updated update script

--- melt-website-update.sh
+++ melt-website-update.sh
@@ -7,12 +7,20 @@ rm -rf .git
 echo $'\nUpdating ruby gems...\n'
 bundle install --path vendor/bundle
 
-# Clone all other repos and remove their .git folders
-echo $'\nPulling updates to the Silver documentation...\n'
+# Clone and build Silver documentation
+echo $'\nBuilding silver documentation\n'
+mkdir scripttemp
+cd scripttemp
+git clone https://github.com/melt-umn/silver.git
+git checkout feature/docgen #This needs to be taken out when we stop developing on the feature branch
 cd silver
-git clone https://github.umn.edu/melt/silver-wiki-jekyll.git doc
-rm -rf !$/.git
-cd ..
+#Compile Silver
+./self-compile --doc --clean
+#Copy the generated documentation into the 
+cp -rf generated/doc/silver documentation/ref/generated
+cp -rf generated/doc/core documentation/ref/generated
+#Copy the entire documentation folder into the Silver folder
+cp -rf documentation/* ../../silver/doc
 
 # TODO: Will need to add repos and then script to do pull
 #echo $'Pulling updates to the Copper documentation...\n'
@@ -28,4 +36,7 @@ python3 menu-builder.py
 # Run jekyll to build the new site
 echo $'\nBuilding with jekyll...\n'
 bundle exec jekyll build --destination ../build # TODO: Change the destination path, or copy from build to the melt root folder
+
+# Kill myself
+
 
