40b82469ead370b99d85aab35938ca59f27f9824
GitBot <evw@umn.edu>
2016-06-06 14:07:31 -0500

    going live

--- step4-run-jekyll.sh
+++ step4-run-jekyll.sh
@@ -29,14 +29,15 @@ echo $'\nBuilding with jekyll...\n'
 
 # TODO: Change the destination path, or copy from build to the melt root folder
 
-rm -Rf ../build
-bundle exec jekyll build --destination ../build 
+rm -Rf /web/research/melt.cs.umn.edu/alpha
+
+bundle exec jekyll build --destination /web/research/melt.cs.umn.edu/alpha
 
 
 if [ -z "$1" ]; then
     # TODO: check that "$1" is actually "--install" instead of just not empty.
     echo ""
-    echo "********  Site installed in default \"build\" directory."
+    echo "********  Site installed in default \"alpha\" directory."
 else
     ./step5-install-site.sh "$1"
 fi
--- step5-install-site.sh
+++ step5-install-site.sh
@@ -7,12 +7,32 @@
 # directory that is just above the 'melt-website' repository
 # directory.
 
-rm -Rf /web/research/melt.cs.umn.edu/alpha
-
-mv ../build /web/research/melt.cs.umn.edu/alpha
+umask 002
 
 cd /web/research/melt.cs.umn.edu
 
-chgrp -R melt alpha/*
-chmod -R g=u alpha/*
+chgrp -R melt alpha
+chmod -R g=u alpha
 chmod -R o=u-w alpha*
+
+# don't overwrite existing js files
+# main_left_nav.js and silver_top_nav.js to be removed later
+cp alpha/js/* js
+
+
+cp -R  alpha/css .
+
+cp alpha/index.html .
+
+cp -R alpha/people .
+
+cp -R alpha/ableC .
+
+cp alpha/silver/index.html silver/
+cp -R alpha/silver/doc/ silver/
+
+cp alpha/copper/index.html copper/
+
+cp alpha/pubs/index.html pubs/
+
+#exit
