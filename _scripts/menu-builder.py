dc892c0100928c49789dcf12b2ed4675f64aaa26
Ted Kaminski <tedinski@cs.umn.edu>
2018-05-17 12:43:10 -0500

    slightly more powerful menu builder

--- _scripts/menu-builder.py
+++ _scripts/menu-builder.py
@@ -3,67 +3,6 @@
 import os
 import re
 
-# TODO: BUG: We're not examining any files OTHER than index.md right now!
-
-# Acceptable directories to traverse, looking for menu information		
-def acceptable_directory(path, f):
-	return not os.path.isfile(os.path.join(path, f)) and \
-	       not f.startswith("_") and \
-	       not f.startswith(".") and \
-	       f != "vendor"
-
-# Examines subdirectories below the given path
-# NOTE: not the path itself!
-def build_path(path, depth):
-	def pretty_path_join(path, f):
-		if path == ".":
-			return f
-		return os.path.join(path, f)
-		
-	subdirs = [f for f in os.listdir(path) if acceptable_directory(path,f)]
-	subitems = [build_menu_item(pretty_path_join(path, f), depth) for f in subdirs]
-	return compile_menu_items(subitems)
-
-# base_dir: the path to examine
-# depth: how deep this is (start at 0 for top level menu items)
-# returns: (weight, string) tuple
-def build_menu_item(base_dir, depth):
-	# Get properties from the index file
-	index_properties = parse_frontmatter(os.path.join(base_dir, "index.md"))
-	# Get menu for all subdirectories
-	submenu = build_path(base_dir, depth + 1)
-	# Get or build the menu items title
-	menu_title = index_properties.get('menu_title', os.path.split(base_dir)[1].replace("-", " ").title())
-	# Get or build the menu items weight
-	weight = float(index_properties.get('menu_weight', 10000))
-	# Get the the menu string for this directory and all subdirectories
-	return (weight, build_menu_item_string(menu_title, depth, base_dir, index_properties, submenu))
-
-# menu_items: list of (weight, string) tuples
-# returns: sorted by weight, each string joined
-def compile_menu_items(menu_items):
-	sorted_menu_items = sorted(menu_items, key=lambda i: i[0])
-	return "".join([i[1] for i in sorted_menu_items])
-	
-# title: the page title
-# depth: the depth in the menu
-# directory: the path containing this file
-# properties: properties for this markdown file
-# submenu_string: the concatenated menuitems for its children
-# returns: a string for this menu item
-def build_menu_item_string(title, depth, directory, properties, submenu_string):
-	has_submenu = "" != submenu_string.strip()
-	if not has_submenu and not properties['valid']:
-		return ""
-	space = "    " * depth
-	menu_string = space + "- text: " + title + "\n" + \
-		      space + "  url: /" + directory + "/\n"
-	if not properties['valid'] or properties.get('menu_nolink', None) == "true":
-		menu_string += space + "  nolink: true\n"
-	if has_submenu:
-		menu_string += space + "  subitems:\n" + submenu_string
-	return menu_string
-
 # md_file_path: a path to a markdown file to examine
 # returns: A dictionary:
 #   present - does this file exist
@@ -78,7 +17,7 @@ def parse_frontmatter(md_file_path):
 		lines = md_file.readlines()
 		props['valid'] = len(lines) > 0 and lines[0] == "---\n"
 		if props['valid']:
-			regex = re.compile(r"^([a-zA-Z0-9_ ]*):(.*)")
+			regex = re.compile(r"^([a-zA-Z0-9_]*) *:(.*)")
 			index = 1
 			while index < len(lines) and not lines[index].startswith("---"):
 				match = regex.match(lines[index])
@@ -88,16 +27,104 @@ def parse_frontmatter(md_file_path):
 					print("ERR: confused by prop in " + md_file_path)
 				index = index + 1
 		else:
-			print("wrn: invalid index " + md_file_path)
+			print("wrn: no frontmatter " + md_file_path)
 	return props
 
 
+class MenuContainer:
+	# self.items = a list of MenuItems
+	def __init__(self, items):
+		self.items = sorted(items, key=lambda i: i.weight)
+	
+	def render(self, depth=0):
+		return ''.join([i.render(depth) for i in self.items])
+	
+	@staticmethod
+	def from_dir(path):
+		"""Returns a ``MenuContainer``s looking at ``path``. Includes all markdown files and all ``subdir/index.md``."""
+		def acceptable_directory(f):
+			return os.path.isdir(os.path.join(path, f)) and \
+			       not f.startswith("_") and \
+			       not f.startswith(".") and \
+			       f != "vendor"
+		def acceptable_mdfile(f):
+			if f == "README.md":
+				return False
+			if f == "index.md" and path != ".":
+				return False
+			return f.endswith(".md")
+		def pretty_join(d, f):
+			if d == ".":
+				return f
+			return os.path.join(d, f)
+		print("MCfrom_dir " + path)
+		contents = os.listdir(path)
+		dirs = [f for f in contents if acceptable_directory(f)]
+		print(", ".join(dirs))
+		mdfiles = [f for f in contents if acceptable_mdfile(f)]
+		
+		d_items = [MenuItem.from_dir(pretty_join(path, f)) for f in dirs]
+		f_items = [MenuItem.from_file(pretty_join(path, f)) for f in mdfiles]
+		return MenuContainer(d_items + f_items)
+
+class MenuItem:
+	# self.path = path to markdown file
+	# self.child = a MenuContainer
+	def __init__(self, path, child):
+		self.path = path
+		self.child = child
+		if path.endswith('index.md'):
+			self.url = path[:-8]
+			fallback_title = os.path.split(path[:-9])[1] # dir name
+			if self.url == "":
+				self.url = ""
+		elif path.endswith('.md'):
+			self.url = path[:-3] + ".html"
+			fallback_title = os.path.split(path[:-3])[1] # file name
+		else:
+			self.url = path
+			print("Unexpected path " + path)
+		self.props = parse_frontmatter(path)
+		self.valid = self.props['valid']
+		self.weight = float(self.props.get('menu_weight', 10000))
+		self.title = self.props.get('menu_title', fallback_title.replace("-", " ").title())
+		self.nolink = self.props.get('menu_nolink', None) == "true"
+	
+	def render(self, depth):
+		if self.child:
+			submenu_string = self.child.render(depth + 1)
+		else:
+			submenu_string = ""
+		# Suppress sub-branches that are not valid
+		has_submenu = ("" != submenu_string.strip())
+		if not has_submenu and not self.valid:
+			return ""
+		space = "    " * depth
+		menu_string = space + "- text: " + self.title + "\n" + \
+			      space + "  url: /" + self.url + "\n"
+		if not self.valid or self.nolink:
+			menu_string += space + "  nolink: true\n"
+		if has_submenu:
+			menu_string += space + "  subitems:\n" + submenu_string
+		return menu_string
+
+	@staticmethod
+	def from_dir(path):
+		print("MIfrom_dir " + path)
+		return MenuItem(os.path.join(path, "index.md"), MenuContainer.from_dir(path))
+	
+	@staticmethod
+	def from_file(path):
+		print("MIfrom_file " + path)
+		return MenuItem(path, None)
+		
+
 # Build the menu
-menu = "- text: Home\n  url: /\n" + build_path(".", 0)
+menu = MenuContainer.from_dir(".")
 
 # Write to the sv_wiki.yml in _data
 with open("_data/sv_wiki.yml", "w") as f:
-	f.write(menu)
+	f.write(menu.render())
 
 print("Menu Generated")
 
